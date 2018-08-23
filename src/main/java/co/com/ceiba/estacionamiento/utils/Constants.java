29cbb268e22f145443cd2b7c1c0e68f82806c7c8
Roger Córdoba García <roger.cordoba@ceiba.com.co>
2018-08-23 18:53:03 -0500

    The project updates the constants

--- src/main/java/co/com/ceiba/estacionamiento/utils/Constants.java
+++ src/main/java/co/com/ceiba/estacionamiento/utils/Constants.java
@@ -4,16 +4,23 @@ public final class Constants {
 	/**
 	 * minimum hours for the day
 	 */
-	public Integer 	HOURS_MIN_FOR_THE_DAY 	= 9;
-	public Integer 	HOURS_OF_THE_DAY 		= 24;
+	public static final Integer HOURS_MIN_FOR_THE_DAY 	= 9;
+	public static final Integer HOURS_OF_THE_DAY 		= 24;
 
-	public Integer 	CANT_MAX_VEHICLES 	= 20;
-	public Double 	COST_HOUR_VEHICLES 	= 1000.0;
-	public Double 	COST_DAY_VEHICLES 	= 8000.0;
+	public static final Integer CANT_MAX_VEHICLES 	= 20;
+	public static final Double 	COST_HOUR_VEHICLES 	= 1000.0;
+	public static final Double 	COST_DAY_VEHICLES 	= 8000.0;
 
-	public Integer 	CANT_MAX_MOTORCYCLES 		= 5;
-	public Double 	COST_HOUR_MOTORCYCLES 		= 500.0;
-	public Double 	COST_DAY_MOTORCYCLES 		= 4000.0;
-	public Double 	COST_SURCHARGE_MOTORCYCLES 	= 2000.0;
-	public Integer 	CYLINDER_MAX_MOTOCYCLES 	= 500;
+	public static final Integer CANT_MAX_MOTORCYCLES 		= 5;
+	public static final Double 	COST_HOUR_MOTORCYCLES 		= 500.0;
+	public static final Double 	COST_DAY_MOTORCYCLES 		= 4000.0;
+	public static final Double 	COST_SURCHARGE_MOTORCYCLES 	= 2000.0;
+	public static final Integer CYLINDER_MAX_MOTOCYCLES 	= 500;
+
+
+
+	private Constants() {
+		throw new IllegalStateException("Contants Utility class");
+	}
+
 }
